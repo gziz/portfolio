6d26665d553d748cc638a08c24d92c2d911a23df
gziz <69287011+gziz@users.noreply.github.com>
2023-07-01 12:32:45 -0600

    Add link button

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,30 +2,16 @@ import React from 'react'
 import { data } from "../data/data.js";
 import {
     FaGithub,
-    FaLink
+    FaLink,
+    FaExternalLinkAlt
   } from 'react-icons/fa';
 
+function openLinks(project) {
+    window.open(project.github, '_blank');
+    //window.open("https://www.github.com/", '_blank');
+}
+
 function Work() {
-
-    const renderDemo = (item) => {
-        if (item.demo !== ""){
-            return <a href={item.demo}>
-                        <button className='flex-col text-center rounded-lg px-4 py-3 m-2'>
-                            <FaLink size={32}/>
-                        </button>
-                    </a>
-        }
-    };
-
-    const renderGithub = (item) => {
-        if (item.github !== ""){
-            return <a href={item.github}>
-                        <button className='flex-col text-center rounded-lg px-4 py-3 m-2'>
-                            <FaGithub size={34}/>
-                        </button>
-                    </a>
-        }
-    };
 
     return (
         <div name='projects' className='w-full bg-[#0a192f]'>
@@ -43,18 +29,30 @@ function Work() {
 
                             <img className="w-full" src={`${process.env.PUBLIC_URL}/assets/projects/${item.imgFile}`} alt="Sunset in the mountains"/>
                             <div className="px-6 py-2">
-                                <div className="font-bold text-xl mb-2 text-gray-300">{item.title}</div>
+                                <div className="flex justify-between items-center my-2">
+                                    <div className="font-bold text-xl text-gray-300">{item.title}</div>
+                                        <a target="_blank" rel="noopener noreferrer" 
+                                            onClick={(e) => {
+                                                e.preventDefault(); 
+                                                openLinks(item);
+                                            }}
+                                            >
+                                                <FaExternalLinkAlt style={{ color: '#fff' }}/>
+                                        </a>
+                                        
+                                </div>
                                 <p className="text-gray-400 text-base">
                                     {item.description}
                                 </p>
                             </div>
-                            <div className="px-4 pt-2 pb-2">
+                            <div className="px-4 py-2">
                                 {item.technologies.map((tech, index) => (
                                     <img className="inline-block w-9 mr-4" src={`${process.env.PUBLIC_URL}/assets/icons/${tech.icon}`} alt={tech.name}/>
                                 ))}
                             </div>
                         </div>
                     ))}
+
 
     
                 </div>
