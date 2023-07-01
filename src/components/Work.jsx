78db13c0f2f1ef942c52d807bcd44f83918bac47
gziz <69287011+gziz@users.noreply.github.com>
2023-07-01 00:24:31 -0600

    Logos working

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -32,7 +32,7 @@ function Work() {
         <div name='projects' className='w-full bg-[#0a192f]'>
     
             <div className='max-w-[1000px] mx-auto  p-8 flex flex-col justify-center w-11/12 h-full'>
-                <div className='text-gray-300 pb-8'>
+                <div className='text-gray-300 pb-8 '>
                     <p className='text-4xl font-bold inline border-b-4 border-sky-300'>Projects</p>
                     <p className='  pt-4'>Check out some of my recent projects</p>
                 </div>
@@ -40,16 +40,19 @@ function Work() {
                 <div className='grid sm:grid-cols-2 md:grid-col-3 gap-5'>
                     
                 {data.map((item, index) => (
-                    <div class="rounded overflow-hidden shadow-2xl bg-white">
+                    <div class="rounded overflow-hidden shadow-lg shadow-[#040c16] bg-[#192231]">
                         <img class="w-full" src={item.image} alt="Sunset in the mountains"/>
-                        <div class="px-6 py-2 border-2 border-t-indigo-500">
-                            <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+                        <div class="px-6 py-2">
+                            <div class="font-bold text-xl mb-2 text-gray-300">{item.name}</div>
+                            <p class="text-gray-400 text-base">
+                                {item.description}
+                            </p>
     
                         </div>
-                        <div class="px-6 pt-2 pb-2">
-                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                        <div class="px-6 pt-2 pb-4">
+                            {item.technologies.map((tech, index) => (
+                                <img class=" inline-block w-10 mr-4" src={tech.icon} alt={tech.name}/>
+                            ))}
                         </div>
                     </div>
     
