606aff8c3f4a5fecc440ef6ba624e6fc88b46c16
gziz <69287011+gziz@users.noreply.github.com>
2024-02-16 22:50:48 -0600

    Add snip notes

--- src/data/data.js
+++ src/data/data.js
@@ -1,87 +1,96 @@
-
-export const data=[
-    {
-        title:"Virtual Assistant Language Model",
-        description:"Scalable AI assistant powered by Chat-GPT model for customer service. The AI extracts information from PDFs (document retrieval). Access is through a telegram bot.",
-        imgFile: "quetzal.gif",
-        technologies: [
-            { name: 'Python', icon: "python.png" },
-            { name: 'OpenAI', icon: "openai2.png"},
-            { name: 'FastAPI', icon: "fastapi.png" },
-            { name: 'Redis', icon: "redis.png" },
-            { name: 'TelegramAPI', icon: "telegram.png" },
-            { name: 'NGINX', icon: "nginx.png" },
-            { name: 'Deployment', icon: "aws.png" },
-
-          ],
-        github:"https://github.com/gziz/llm-chatbot-for-banking",
-        demo:""
-    },
-    {
-      title:"IoT API with Crypto Algorithms",
-      description:"IoT API with mutual authentication and cryptographic algorithms implemented from scratch for digital signature validation (ECDSA).",
-      imgFile: "crypto_iot.png",
-      technologies: [
-          { name: 'Python', icon: "python.png" },
-          { name: 'FastAPI', icon: "fastapi.png" },
-          { name: 'Postgres', icon: "postgres.png" },
-          { name: 'NGINX', icon: "nginx.png" },
-          { name: 'Docker', icon: "docker.png" },
-          { name: 'Deployment', icon: "aws.png" },
-          
-        ],
-      github:"https://github.com/gziz/iot-crypto",
-      demo:""
+export const data = [
+  {
+    title: "Note taking inside vscode extension",
+    description:
+      "Just highlight a code segment, add your note, and then view them organized in your sidebar.",
+    imgFile: "snip-notes.png",
+    technologies: [
+      { name: "Typescript", icon: "typescript.svg" },
+      { name: "NodeJs", icon: "node.png" },
+    ],
+    github: "https://github.com/gziz/snip-notes",
+    demo: "https://snip-notes-web.vercel.app/",
   },
   {
-      title:"Question Answering AI",
-      description:"A text and question are provided, by implementing a deep learning transformers architecture, the model will extract the answer from the aforementioned text.",
-      imgFile: "qa.png",
-      technologies: [
-        { name: 'Python', icon: "python.png" },
-        { name: 'HuggingFace', icon: "huggingface.png" },
-        { name: 'FastAPI', icon: "fastapi.png" },
-        { name: 'JavaScript', icon: "js.png" },
-        { name: 'React', icon: "react.png" },
-        { name: 'Docker', icon: "docker.png" },
-        { name: 'Deployment', icon: "aws.png" },
-      ],
-      github:"https://github.com/gziz/question-answer-ai",
-      demo:"https://questionanswer.gerardomz.com/",
+    title: "Virtual Assistant Language Model",
+    description:
+      "Scalable AI assistant powered by Chat-GPT model for customer service. The AI extracts information from PDFs (document retrieval). Access is through a telegram bot.",
+    imgFile: "quetzal.gif",
+    technologies: [
+      { name: "Python", icon: "python.png" },
+      { name: "OpenAI", icon: "openai2.png" },
+      { name: "FastAPI", icon: "fastapi.png" },
+      { name: "Redis", icon: "redis.png" },
+      { name: "TelegramAPI", icon: "telegram.png" },
+      { name: "NGINX", icon: "nginx.png" },
+      { name: "Deployment", icon: "aws.png" },
+    ],
+    github: "https://github.com/gziz/llm-chatbot-for-banking",
+    demo: "",
   },
-    {
-        title:"Vehicle Routing Optimization",
-        description:"Optimize traveling distance for profit maximization given user selected locations. Implemented heuristic from scratch (Altered Nearest Neighbor).",
-        imgFile: "tsp.png",
-        technologies: [
-          { name: 'Python', icon: "python.png" },
-          { name: 'JavaScript', icon: "js.png" },
-          { name: 'React', icon: "react.png" },
-          { name: 'Heroku', icon: "heroku.png" },
-        ],
-        github:"https://github.com/gziz/tsp-front",
-        demo:"",
-    },
-    {
-        title:"Monte Carlo Ruin Simulation",
-        description:"Analyzed historical insurance data and performed goodness-of-fit tests to obtain random variable parameters for simulation.",
-        imgFile: "montecarlo.png",
-        technologies: [
-          { name: 'Python', icon: "python.png" },
-        ],
-        github:"https://github.com/gziz/cl-ruin-simulation",
-        demo:"https://gziz-cl-ruin-simulation-appmain-04h648.streamlitapp.com/",
-    },
-    {
-        title:"Mars Search Algorithms",
-        description:"Evaluate the performance of multiple search algorithms in a Mars simulated environment.",
-        imgFile: "mars.png",
-        technologies: [
-          { name: 'Python', icon: "python.png" },
-        ],
-        github:"https://github.com/gziz/mars-routes",
-        demo:"",
-    },
-
-
-]
+  {
+    title: "IoT API with Crypto Algorithms",
+    description:
+      "IoT API with mutual authentication and cryptographic algorithms implemented from scratch for digital signature validation (ECDSA).",
+    imgFile: "crypto_iot.png",
+    technologies: [
+      { name: "Python", icon: "python.png" },
+      { name: "FastAPI", icon: "fastapi.png" },
+      { name: "Postgres", icon: "postgres.png" },
+      { name: "NGINX", icon: "nginx.png" },
+      { name: "Docker", icon: "docker.png" },
+      { name: "Deployment", icon: "aws.png" },
+    ],
+    github: "https://github.com/gziz/iot-crypto",
+    demo: "",
+  },
+  {
+    title: "Question Answering AI",
+    description:
+      "A text and question are provided, by implementing a deep learning transformers architecture, the model will extract the answer from the aforementioned text.",
+    imgFile: "qa.png",
+    technologies: [
+      { name: "Python", icon: "python.png" },
+      { name: "HuggingFace", icon: "huggingface.png" },
+      { name: "FastAPI", icon: "fastapi.png" },
+      { name: "JavaScript", icon: "js.png" },
+      { name: "React", icon: "react.png" },
+      { name: "Docker", icon: "docker.png" },
+      { name: "Deployment", icon: "aws.png" },
+    ],
+    github: "https://github.com/gziz/question-answer-ai",
+    demo: "https://questionanswer.gerardomz.com/",
+  },
+  {
+    title: "Vehicle Routing Optimization",
+    description:
+      "Optimize traveling distance for profit maximization given user selected locations. Implemented heuristic from scratch (Altered Nearest Neighbor).",
+    imgFile: "tsp.png",
+    technologies: [
+      { name: "Python", icon: "python.png" },
+      { name: "JavaScript", icon: "js.png" },
+      { name: "React", icon: "react.png" },
+      { name: "Heroku", icon: "heroku.png" },
+    ],
+    github: "https://github.com/gziz/tsp-front",
+    demo: "",
+  },
+  {
+    title: "Monte Carlo Ruin Simulation",
+    description:
+      "Analyzed historical insurance data and performed goodness-of-fit tests to obtain random variable parameters for simulation.",
+    imgFile: "montecarlo.png",
+    technologies: [{ name: "Python", icon: "python.png" }],
+    github: "https://github.com/gziz/cl-ruin-simulation",
+    demo: "https://gziz-cl-ruin-simulation-appmain-04h648.streamlitapp.com/",
+  },
+  // {
+  //   title: "Mars Search Algorithms",
+  //   description:
+  //     "Evaluate the performance of multiple search algorithms in a Mars simulated environment.",
+  //   imgFile: "mars.png",
+  //   technologies: [{ name: "Python", icon: "python.png" }],
+  //   github: "https://github.com/gziz/mars-routes",
+  //   demo: "",
+  // },
+];
